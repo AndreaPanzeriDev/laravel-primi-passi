8c07f827edd1bd8852f2b8e0fc2c329de2b63c07
AndreaPanzerDev <andpanz@libero.it>
2023-01-11 18:05:55 +0100

    finish

---
+++ laravel-primi-passi/resources/views/aboutMe.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title></title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="css/style.css" rel="stylesheet">
+</head>
+
+<body>
+    <header>
+        <h1>AboutMe</h1>
+
+        <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/console">Console</a></li>
+            <li><a href="/output">Output</a></li>
+        </ul>
+
+    </header>
+</body>
+
+</html>
